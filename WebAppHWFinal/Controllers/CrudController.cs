bf3023872b4506a2d723e754232359c3ef65f6dd
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-01-21 18:05:10 +0300

    создание пустого контроллера

---
+++ WebAppHWFinal/Controllers/CrudController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppHWFinal.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        public CrudController()
+        {
+
+        }
+    }
+}
