770958ac039fae219eb711c3e6bebc022907fb78
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-01-21 22:00:22 +0300

    Заполнены методы ValuesHolder и контроллера

--- WebAppHWFinal/Controllers/CrudController.cs
+++ WebAppHWFinal/Controllers/CrudController.cs
@@ -7,33 +7,45 @@ namespace WebAppHWFinal.Controllers
     [ApiController]
     public class CrudController : ControllerBase
     {
-        public CrudController()
+        private readonly ValuesHolder _holder;
+        public CrudController(ValuesHolder holder)
         {
+            _holder = holder;
+        }
+        
+        [HttpPost("create/{str}")]
+        public IActionResult Create([FromQuery] string str)
+        {
+            _holder.Add(str);
+            return Ok();
+        }
+        
+        [HttpGet("read/{num}")]
+        public IActionResult Read([FromQuery] int num)
+        {
+            string temp = _holder.Get(num)!;
+            if (temp is not null)
+                return Ok(temp);
+            else
+                return NotFound();
+        }
+        
+        [HttpPut("update/{num}/{str}")]
+        public IActionResult Update([FromQuery] string str, [FromQuery] int num)
+        {
+            if (_holder.Update(num,str))
+                return Ok("updated");
+            else
+                return NotFound();
+        }
 
-        }
-        
-        [HttpPost("create")]
-        public IActionResult Create()
+        [HttpDelete("delete/{num}")]
+        public IActionResult Delete([FromQuery] int num)
         {
-            return Ok();
-        }
-        
-        [HttpGet("read")]
-        public IActionResult Read()
-        {
-            return Ok();
-        }
-        
-        [HttpPut("update")]
-        public IActionResult Update()
-        {
-            return Ok();
-        }
-
-        [HttpDelete("delete")]
-        public IActionResult Delete()
-        {
-            return Ok();
+            if (_holder.Delete(num))
+                return Ok("Deleted");
+            else
+                return NotFound();
         }
     }
 }
---
+++ WebAppHWFinal/ValuesHolder.cs
@@ -0,0 +1,34 @@
+﻿namespace WebAppHWFinal
+{
+    public class ValuesHolder
+    {
+        private List<string> _values { get; set; }
+        public ValuesHolder()
+        {
+            _values = new List<string>();
+        }
+        public void Add(string value) => _values.Add(value);
+        public string? Get(int count) => _values.Count > count ? _values[count] : null;
+        public bool Delete(int count)
+        {            
+            if (Get(count) is not null)
+            {
+                _values.RemoveAt(count);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Update(int count, string str)
+        {
+            if (Get(count) is not null)
+            {
+                _values[count] = str;
+                return true;
+            }
+            return false;
+        }
+    }
+}
