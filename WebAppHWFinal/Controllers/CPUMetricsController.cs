d86bfa2da46d0800d4e3722c69823a9f164bde78
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-01-25 19:28:33 +0300

    logger встроен в CPUMetricsController

--- WebAppHWFinal/Controllers/CPUMetricsController.cs
+++ WebAppHWFinal/Controllers/CPUMetricsController.cs
@@ -7,6 +7,13 @@ namespace MetricsManager.Controllers;
 [ApiController]
 public class CPUMetricsController : ControllerBase
 {
+    private readonly ILogger<CPUMetricsController> _logger;
+
+    public CPUMetricsController(ILogger<CPUMetricsController> logger)
+    {
+        _logger = logger;
+        _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+    }
     [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
     public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
     {
