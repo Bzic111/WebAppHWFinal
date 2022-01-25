3ebdb4ff999820bd9ab265762b7a68490898ba15
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-01-25 17:01:21 +0300

    Добавление контроллеров метрик и пустых методов

---
+++ WebAppHWFinal/Controllers/DotNetMetricsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppHWFinal.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DotNetMetricsController : ControllerBase
+{
+
+    [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+    public IActionResult GetMetricsFromAgent([FromRoute] int agentId,[FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+    [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+    public IActionResult GetMetricsFromCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+}
---
+++ WebAppHWFinal/Controllers/HDDMetricsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppHWFinal.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HDDMetricsController : ControllerBase
+{
+    [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+    public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+    [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+    public IActionResult GetMetricsFromCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+}
