991c0d6f6ff2ae010f4c600294682d78a0a7826e
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-01-25 19:25:10 +0300

    добавлен и настроен NLog.config

--- WebAppHWFinal/Program.cs
+++ WebAppHWFinal/Program.cs
@@ -1,17 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
+using NLog.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+try
+{
+    logger.Debug("init main");
+    CreateHostBuilder(args).Build().Run();
+}
+catch (Exception exception)                                                     // отлов всех исключений в рамках работы приложения
+{
+                                                                                //NLog: устанавливаем отлов исключений
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+                                                                                // остановка логера
+    NLog.LogManager.Shutdown();
+}
+static IHostBuilder CreateHostBuilder(string[] args) =>
+Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+{
+    webBuilder.UseStartup<IStartup>();
+})
+.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();                                                   // создание провайдеров логирования
+    logging.SetMinimumLevel(LogLevel.Trace);                                    // устанавливаем минимальный уровень логирования
+}).UseNLog();
+
+                                                                                // Add services to the container.
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                                                                                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+                                                                                // Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
 {
