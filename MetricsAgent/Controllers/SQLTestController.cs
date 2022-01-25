8f91a0f0edf7058536caf9b508e805d67787334c
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-01-25 19:34:53 +0300

    added sqltest controller

---
+++ MetricsAgent/Controllers/SQLTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+
+namespace MetricsAgent.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SQLTestController : ControllerBase
+{
+    [HttpGet("sql-test")]
+    public IActionResult TryToSqlLite()
+    {
+        string cs = "Data Source=:memory:";
+        string stm = "SELECT SQLITE_VERSION()";
+        using (var con = new SQLiteConnection(cs))
+        {
+            con.Open();
+            using var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+            return Ok(version);
+        }
+    }
+}
